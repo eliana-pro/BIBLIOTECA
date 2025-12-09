98e9bdb8f189c422a5574382876a94b9bd9db5c4
Eliana Alejandra Gamboa Velandia <1206velandia.g@gmail.com>
2025-12-08 20:09:43 -0500

    Arreglos del codigo

--- components/bucv_footer.php
+++ components/bucv_footer.php
@@ -1,3 +1,3 @@
 <footer style="background: #333; color: white; padding: 20px; text-align: center; margin-top: 50px;">
-    <p>&copy; 2024 Biblioteca UNICAB - Todos los derechos reservados</p>
+    <p>&copy; 2025 Biblioteca UNICAB - Todos los derechos reservados</p>
 </footer>
